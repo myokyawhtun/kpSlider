47747cfc5d907ac2aabe1bb5c894865667191f93
Myo Kyaw Htun <myochauhtun@hotmail.com>
2013-06-10 14:54:14 +0700

    added next previous
    Added feature to show next/previous and navigation bullets hide feature

--- jquery.kpslider.js
+++ jquery.kpslider.js
@@ -3,17 +3,24 @@
 	$.fn.kpSlider = function(options){
 		
 		var numItems = 3; // default items to be shown
+		var navPrevNext = false; // default previous / next navigation
 		var total = 0;
+		
 		var bullets = 0;
+		var navBullets = true; // default show bullets
 
 		var totalWidth = 0;
 
 		var itemWidth = 0;
 		var itemHeight = 0;
 
+		var currentItem = 0;
+
 		// default settings
 		var settings = $.extend({
-			displayItems: numItems
+			displayItems: numItems,
+			showPrevNext: navPrevNext,
+			showBullets: navBullets
 		}, options);
 		
 		numItems = settings.displayItems; // set the items number to be shown
@@ -32,6 +39,13 @@
 
 		});
 
+		// show previous / next navigation
+		if(settings.showPrevNext==true){
+			
+			this.prepend("<div class='navNext'><a href='#'>Next</a></div>");
+			this.prepend("<div class='navPrev'><a href='#'>Previous</a></div>");
+		}
+
 		if(total>numItems){
 			if (total % numItems ==0){
 				bullets = Math.floor(total/numItems) ;
@@ -39,7 +53,7 @@
 				bullets = Math.floor(total/numItems) +1;
 			}
 			var ulBullet = $("<ul>"); // bullets 
-			ulBullet.addClass("navScroll");
+			ulBullet.addClass("navSlider");
 			for(var b=1;b<=bullets;b++){
 				var li = $("<li>"); // new li bullet
 				var anc = $("<a>");
@@ -50,23 +64,58 @@
 				li.append(anc);
 				ulBullet.append(li);		// append in ul
 			}
-
-			this.prepend(ulBullet); // prepand before all child elements
-
+			if(settings.showBullets==true){ // show bullets only when true
+				this.prepend(ulBullet); // prepand before all child elements
+			}
 		}else{
 			bullets=0;
 		}
-		
+
 		this.css('width',Math.floor(itemWidth * numItems)); // set main layout width multiply by display number 
 
 		this.css('overflow','hidden'); // just hidden the others * should set in CSS
 		this.css('position','relative'); // * should set in CSS
+
+		// previous click
+		this.find('.navPrev a').live('click',function(e){
+			
+			e.preventDefault();
+
+			currentItem = currentItem - 1;
+
+			if(currentItem<=0){
+				currentItem = 0;
+			}
+
+			moveItems(currentItem);
+		});
+		// next click
+		this.find('.navNext a').live('click',function(e){
+			
+			e.preventDefault();
+
+			currentItem = currentItem + 1;
+
+			if(currentItem>=total-1){
+				currentItem = total-1;
+			}
+
+			moveItems(currentItem);
+		});
 
 		this.find('.bullet').live('click',function(e){
 
 			e.preventDefault(); // prevent default anchor click # action
 
 			var rel = $(this).attr('rel');
+
+			currentItem = rel;
+
+			moveItems(currentItem);
+		});
+
+		function moveItems (rel) {
+			// body...
 			var startX = (-(rel * itemWidth)) ; // selected bullet ID multiply by itemWidth 
 			var prevX = startX;
 
@@ -76,8 +125,7 @@
 				},400);
 				prevX +=  itemWidth;
 			});
-
-		});
+		}
 	};
 
 }(jQuery));
